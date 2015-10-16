86c0883ad1387dba0eb1373d156f81c3f4444315
Paul Olthof <hpolthof@gmail.com>
2015-10-16 09:20:39 +0200

    Ionic CDN constantly fails to load and icons are not in use.

--- resources/views/layout/_style.blade.php
+++ resources/views/layout/_style.blade.php
@@ -1,6 +1,5 @@
 <link href="{{ $assetPath }}/css/bootstrap.min.css" rel="stylesheet">
 <link href="{{ $assetPath }}/css/font-awesome.min.css" rel="stylesheet">
-<link href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css" />
 <link href="{{ $assetPath }}/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
 <link href="{{ $assetPath }}/css/skins/skin-{{ \Config::get('admin.layout.skin') }}.min.css" rel="stylesheet" type="text/css" />
 <link href="{{ $assetPath }}/plugins/datepicker/datepicker3.css" rel="stylesheet" type="text/css" />
