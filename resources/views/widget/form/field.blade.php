012748bf42323d52e07f5e9798c9d63bdcce1623
Paul Olthof <hpolthof@gmail.com>
2015-10-27 10:47:46 +0100

    Layout fix

--- resources/views/widget/form/field.blade.php
+++ resources/views/widget/form/field.blade.php
@@ -7,7 +7,7 @@
         @endif
     @else
         <label class="control-label col-sm-3" for="field_{{ $uid }}">{{ $field->getTitle() }} {{ ($field->isRequired()?'*':'') }}</label>
-        <div class="col-sm-9">
+        <div class="col-sm-9" style="padding-bottom: 3px">
             {!! $field->renderField('field_'.$uid) !!}
             @if(count($field->getErrors()) > 0)
                 <p class="help-block">{{ $field->getErrors()[0] }}</p>
