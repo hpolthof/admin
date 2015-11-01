9056b399b6df0a22bc54baa7581af67041dd671b
Paul Olthof <hpolthof@gmail.com>
2015-11-01 08:37:25 +0100

    Prevent doubles

--- resources/views/layout/_scripts.blade.php
+++ resources/views/layout/_scripts.blade.php
@@ -4,7 +4,7 @@
 <script src="{{ $assetPath }}/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
 <script src="{{ $assetPath }}/plugins/fastclick/fastclick.min.js" type="text/javascript"></script>
 <script src="{{ $assetPath }}/plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>
-@foreach(\Config::get('admin.includes.js') as $script)
+@foreach(array_unique(\Config::get('admin.includes.js')) as $script)
     <script src="{{ $script }}" type="text/javascript"></script>
 @endforeach
 @foreach($_ctrl->getJs()->unique() as $script)
